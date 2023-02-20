285d9da4f42ed3acbe48af96c37751a13ab80e76
Dicarbene <1041641731@qq.com>
2023-02-20 16:39:56 +0800

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,4 @@
 cd vuehr
 npm run build
 rm -r ../vhr/vhrserver/vhr-web/src/main/resources/static/*
-cp -r dist/* ../vhr/src/main/resources/static
+cp -r dist/* ../vhr/vhrserver/vhr-web/src/main/resources/static
